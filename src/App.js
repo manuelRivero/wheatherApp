caae7512a0e5fa68e1613e3a39b7260a5230e175
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-20 19:47:18 -0300

    [refactor] add new app layout with material grid and cards

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // material
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper';
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
 
-// Components 
-import Header from './Components/Header/Header'
-import LocationListContainer from './Containers/LocationListContainer';
-import ForecastExtendedContainer from './Containers/ForecastExtendedContainer';
-import './App.css';
+// Components
+import Header from "./Components/Header/Header";
+import LocationListContainer from "./Containers/LocationListContainer";
+import ForecastExtendedContainer from "./Containers/ForecastExtendedContainer";
+import "./App.css";
 
-export default class App extends Component {
-  render() {
-    return (
-      
-      <Container maxWidth="md">
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <Paper >xs=6</Paper>
-              <Paper >xs=6</Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper >xs=6</Paper>
-            </Grid>
+const useStyles = makeStyles(theme => {
+  return {
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: "1rem",
+      textAlign: "center"
+    },
+    container:{
+      marginTop:'80px'
+    }
+  };
+});
+
+export default () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Header />
+      <Container className={classes.container} maxWidth="md">
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>
+              <LocationListContainer />
+            </Paper>
+            <Paper className={classes.paper}></Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>
+              <ForecastExtendedContainer />
+            </Paper>
+          </Grid>
         </Grid>
       </Container>
+    </div>
+  );
+};
 
-    );
-  }
-}
 
-
-/*
-
-         <Grid container spacing={2}>
-              <Grid item xs={12}>
-                Header
-              </Grid>
-              <Grid item xs={12}>
-                  <LocationListContainer />
-              </Grid>
-              <Grid item xs={12}>
-                <ForecastExtendedContainer />
-              </Grid>
-          </Grid>
-*/
