89bb989ef0358ce944e50fbf880c759bb1584eac
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-21 12:52:33 -0300

    [add] set up user location actions
    load weather for current user location

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -2,3 +2,4 @@ export const SET_CITY = "SET_CITY!";
 export const GET_CITIES = "GET_CITIES";
 export const SET_FORECAST_DATA = "SET_FORECAST_DATA";
 export const ADD_CITY ="ADD_CITY";
+export const SET_USER_LOCATION="SET_USER_LOCATION"
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import { getData, transformForecastExtend } from "../services/TransformWeather";
-import {SET_CITY,SET_FORECAST_DATA, ADD_CITY} from './ActionTypes';
+import {SET_CITY,SET_FORECAST_DATA, ADD_CITY, SET_USER_LOCATION} from './ActionTypes';
 const setCity = payload => ({ type: SET_CITY, payload });
 const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload });
 
@@ -26,3 +26,17 @@ export const addCity = city => {
 
   })
 }
+export const setUserLocation = ()=>{
+  let locationUrl = "http://www.geoplugin.net/json.gp";
+  return (dispatch =>{
+    fetch(locationUrl)
+    .then(res => res.json())
+    .then(({geoplugin_city, geoplugin_regionName, geoplugin_currencyCode }) =>{
+      // set city in correct format for wheather api ej ( " rosario , ar ")
+      let formatedCityInfo = [geoplugin_city, geoplugin_currencyCode].join(",")
+      dispatch(addCity(formatedCityInfo))
+      dispatch(setSelectedCity(formatedCityInfo))
+    })
+  })
+}
+
