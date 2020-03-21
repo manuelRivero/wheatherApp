14eb332173bdb00e8e1f37e2b6156835b63ee6d5
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-21 12:55:54 -0300

    [npm] add material ui icons

--- src/Containers/LocationListContainer.js
+++ src/Containers/LocationListContainer.js
@@ -1,42 +1,47 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 // redux
-import {connect} from 'react-redux';
-import {setSelectedCity} from '../actions';
+import { connect } from "react-redux";
+import { setSelectedCity, setUserLocation } from "../actions";
 // components
-import WeatherList from '../Components/Weather/WeatherList/WeatherList'
+import WeatherList from "../Components/Weather/WeatherList/WeatherList";
 
-class ForecastListContainer extends Component{
+class ForecastListContainer extends Component {
   static propTypes = {
     dispatchsetCity: PropTypes.func.isRequired
-  }
+  };
 
-  
-  cities = ["Lima,pe", "Chimbote,pe", "Trujillo,pe"]
-  
-  handleSelectionLocation = (city)=> {
-    this.setState({
-        city
-    });
+  handleSelectionLocation = city => {
     this.props.dispatchsetCity(city);
-};
+  };
+
+  componentDidMount(){
+    this.props.setUserLocation()
+  }
 
   render() {
     return (
       <div>
-        <WeatherList cities={this.cities} selectCity={  this.handleSelectionLocation}/>
+        <WeatherList
+          cities={this.props.cities}
+          selectCity={this.handleSelectionLocation}
+        />
       </div>
-    )
+    );
   }
 }
 
 ForecastListContainer.propsType = {
-    setCity: PropTypes.func.isRequired,
-}
+  setCity: PropTypes.func.isRequired
+};
 
-const mapDispatchToProps = dispatch=>(
-  {
-    dispatchsetCity: city => dispatch(setSelectedCity(city))
-  }
-)
-export default connect(null, mapDispatchToProps)(ForecastListContainer); 
+const mapDispatchToProps = dispatch => ({
+  dispatchsetCity: city => dispatch(setSelectedCity(city)),
+  setUserLocation: () => dispatch(setUserLocation())
+});
+
+const mapStateToProps = ({ cityReducer }) => ({
+  cities: cityReducer.cities
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ForecastListContainer);
