31fc7ef93f68919b7d2b45aa396494fe0a08c8ee
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-21 11:30:54 -0300

    [fix] missing forecast type in getData call
    to make the request to the extended forecast of the selected city

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,28 @@
-import {getData, transformForecastExtend} from '../services/TransformWeather';
-export const SET_CITY = 'SET_CITY!';
-export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
-const setCity = payload => ({type:SET_CITY, payload});
-const setForecastData = payload => ({type:SET_FORECAST_DATA, payload});
+import { getData, transformForecastExtend } from "../services/TransformWeather";
+import {SET_CITY,SET_FORECAST_DATA, ADD_CITY} from './ActionTypes';
+const setCity = payload => ({ type: SET_CITY, payload });
+const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload });
 
+export const setSelectedCity = payload => {
+  return dispatch => {
+    //activar indicador de busqueda
+    dispatch(setCity(payload));
+    getData(payload, "forecast")
+      .then(forecastData => {
+        //modificar el estado con el resultado del fecht
+        console.log(forecastData)
+        const forecastExtend = transformForecastExtend(forecastData);
+        console.log(forecastExtend)
+        dispatch(setForecastData({ city: payload, forecastExtend }));
+      })
+      .catch(err => console.log(`${err}`));
+  };
+};
 
-export const setSelectedCity = payload =>{
-    return dispatch =>{
-        //activar indicador de busqueda
-        dispatch(setCity(payload));
-        getData(payload).then(forecastData => {
-            //modificar el estado con el resultado del fecht
-            const forecastExtend= transformForecastExtend(forecastData);
-            dispatch(setForecastData({city:payload, forecastExtend}));
-          }).catch(err => console.log(`${err}`))
-    }
+export const addCity = city => {
+  return ({
+    type:ADD_CITY,
+    payload:city
+
+  })
 }
