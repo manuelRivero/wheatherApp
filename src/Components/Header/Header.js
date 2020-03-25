7f2ee81d5576394a83decc6e5da3553319799d0b
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-25 13:07:25 -0300

    [add] header menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,38 +1,73 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
 
-import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Menu,
+  MenuItem
+} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 export default function Header() {
-    const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const comingSoon=()=>{
+    alert("Coming Soon")
+    handleClose();
+  }
+
+  const classes = useStyles();
 
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             WeatherApp
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+            variant="contained" color="primary"
+          >
+            Menu
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={comingSoon}>Profile</MenuItem>
+            <MenuItem onClick={comingSoon}>My account</MenuItem>
+            <MenuItem onClick={comingSoon}>Logout</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </>
