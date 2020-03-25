aaf7b8d9273824fd40407ded309d972f094bdf06
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-25 11:26:35 -0300

    Updates

---
+++ precache-manifest.a3c01fe0d2beb61929c05f37c82186b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "412e618e377653988b43",
+    "url": "/wheatherApp/static/js/runtime~main.412e618e.js"
+  },
+  {
+    "revision": "82d20fe51847c5a3cd05",
+    "url": "/wheatherApp/static/js/main.82d20fe5.chunk.js"
+  },
+  {
+    "revision": "0493ff8d5c9c016e837d",
+    "url": "/wheatherApp/static/js/1.0493ff8d.chunk.js"
+  },
+  {
+    "revision": "82d20fe51847c5a3cd05",
+    "url": "/wheatherApp/static/css/main.653216da.chunk.css"
+  },
+  {
+    "revision": "1a8bdecb024f2dee361b48d63722dec0",
+    "url": "/wheatherApp/index.html"
+  }
+];
