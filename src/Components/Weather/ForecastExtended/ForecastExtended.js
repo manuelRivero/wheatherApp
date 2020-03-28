09f6a3bb0af6e1c0b182c9e4cda93dd6bf643ce5
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-28 18:31:04 -0300

    [add] expansion panel on forecastExtended

--- src/Components/Weather/ForecastExtended/ForecastExtended.js
+++ src/Components/Weather/ForecastExtended/ForecastExtended.js
@@ -1,39 +1,89 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ForescastItem from "../ForecastItem/ForecastItem";
-import { CircularProgress, Grid, Paper } from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
+import {
+  CircularProgress,
+  Grid,
+  Paper,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Typography
+} from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles( theme => {
-  return ({
-    paper:{
-      padding:'1rem',
+const useStyles = makeStyles(theme => {
+  return {
+    paper: {
+      padding: "1rem",
       textAlign: "center"
+    },
+    expansionPanelDetail:{
+      flexDirection:'column',
+      maxHeight:'200px',
+      overflow:'scroll'
     }
-  })
-})
+  };
+});
 
-const ForecastExtended = ({ city,forecastExtendedData }) => {
-
-  const classes=useStyles();
+const ForecastExtended = ({ city, forecastExtendedData }) => {
+  const classes = useStyles();
   let forecast = <CircularProgress />;
 
-  if (forecastExtendedData && forecastExtendedData[0] === city ) {
-    forecast = forecastExtendedData[1].map((day, index) => (
-      <ForescastItem
-        weekDay={day.weekDay}
-        hour={day.hour}
-        data={day.data}
-        key={index}
-      ></ForescastItem>
-    ));
-  }
+  if (forecastExtendedData && forecastExtendedData[0] === city) {
+
+    // it's an object
+    let forecastGroups = forecastExtendedData[1].reduce((weekDay, a) => {
+      weekDay[a.weekDay] = [...(weekDay[a.weekDay] || []), a];
+      return weekDay;
+    }, {});
+
+    // converting into an array of groups objects
+    let groupArray=[]
+    for(let group in forecastGroups){
+      groupArray.push({[group]:forecastGroups[group]})
+    } 
+
+    forecast= groupArray.map((dayGroup, groupIndex) => {
+      let dayGroupName;
+      for(let name in dayGroup){
+        dayGroupName=name
+      }
+      return (
+        <ExpansionPanel key={dayGroupName + groupIndex}>
+            <ExpansionPanelSummary
+              key={dayGroupName+groupIndex}
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={dayGroupName}
+              id={dayGroupName}
+            >
+                <Typography>{dayGroupName}</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails className={classes.expansionPanelDetail}>
+              {dayGroup[dayGroupName].map( (day, index)=>{
+                return(
+                  <ForescastItem
+                  weekDay={day.weekDay}
+                  hour={day.hour}
+                  data={day.data}
+                  key={day+index}
+                ></ForescastItem>
+                )
+              })}
+            </ExpansionPanelDetails>
+        </ExpansionPanel>
+        )
+    });
+   }
 
   return (
     <Grid item sm={12} md={6}>
       <Paper className={classes.paper}>
         <h1>{city}</h1>
-      {forecast}
+        {forecast && 
+          forecast
+          }
       </Paper>
     </Grid>
   );
